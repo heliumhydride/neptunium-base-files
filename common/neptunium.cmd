e4d833c6faf212c0acce4b23fbefc45fbddf4081
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-10-31 20:37:10 +0100

    make neptunium launcher show title and icon more universally

--- common/neptunium.cmd
+++ common/neptunium.cmd
@@ -1,4 +1,4 @@
 @echo off
 set NP_PREFIX=%~dp0
-"%NP_PREFIX%\bin\conemu.cmd" -Icon %NP_PREFIX%\neptunium64.ico -Title "Neptunium Development Environment (amd64)" -LoadCfgFile "%NP_PREFIX%\etc\conemu-neptunium.xml" -run "%NP_PREFIX%\neptunium_here.cmd"
+"%NP_PREFIX%\bin\conemu.cmd" -Icon %NP_PREFIX%\neptunium.ico -Title "Neptunium Development Environment (%PROCESSOR_ARCHITECTURE%)" -LoadCfgFile "%NP_PREFIX%\etc\conemu-neptunium.xml" -run "%NP_PREFIX%\neptunium_here.cmd"
 exit
